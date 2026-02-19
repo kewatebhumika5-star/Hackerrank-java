dcc73920065111f0c16a50265833253ab2958610
Bhumika Kewate <kewatebhumika5@gmail.com>
2026-02-19 18:54:00 +0530

    Create java-currency-formatter.java

---
+++ java-currency-formatter.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        // US Locale
+        NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        String us = usFormat.format(payment);
+        
+        // India Locale (custom)
+        Locale indiaLocale = new Locale("en", "IN");
+        NumberFormat indiaFormat = NumberFormat.getCurrencyInstance(indiaLocale);
+        String india = indiaFormat.format(payment);
+        
+        // China Locale
+        NumberFormat chinaFormat = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = chinaFormat.format(payment);
+        
+        // France Locale
+        NumberFormat franceFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String france = franceFormat.format(payment);
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
