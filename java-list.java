d62a16502189348fefaa112bbcf1e5e8fbc98e5e
Bhumika Kewate <kewatebhumika5@gmail.com>
2026-03-08 23:34:28 +0530

    Create java-list.java

---
+++ java-list.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();  
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            list.add(sc.nextInt());
+        }
+
+        int q = sc.nextInt();
+
+        for(int i = 0; i < q; i++){
+            String query = sc.next();
+
+            if(query.equals("Insert")){
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                list.add(x, y);
+            }
+            else if(query.equals("Delete")){
+                int x = sc.nextInt();
+                list.remove(x);
+            }
+        }
+
+        for(int num : list){
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}
